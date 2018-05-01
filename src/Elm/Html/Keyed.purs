07d693e5e81818dbe298b2d5f064f4bd7e90e8b4
Marcelo Lazaroni <lazaronijunior@gmail.com>
2018-05-01 15:58:37 +0200

    Add Elm.Html.Keyed module

---
+++ src/Elm/Html/Keyed.purs
@@ -0,0 +1,46 @@
+module Elm.Html.Keyed ( node , ol , ul ) where
+
+{-| A keyed node helps optimize cases where children are getting added, moved,
+removed, etc. Common examples include:
+
+  - The user can delete items from a list.
+  - The user can create new items in a list.
+  - You can sort a list based on name or date or whatever.
+
+When you use a keyed node, every child is paired with a string identifier. This
+makes it possible for the underlying diffing algorithm to reuse nodes more
+efficiently.
+
+# Keyed Nodes
+@docs node
+
+# Commonly Keyed Nodes
+@docs ol, ul
+-}
+
+
+import Elm.Html (Attribute, Html)
+import Elm.Native.VirtualDom as VirtualDom
+import Data.Tuple (Tuple)
+
+
+{-| Works just like `Html.node`, but you add a unique identifier to each child
+node. You want this when you have a list of nodes that is changing: adding
+nodes, removing nodes, etc. In these cases, the unique identifiers help make
+the DOM modifications more efficient.
+-}
+node :: forall msg. String -> Array (Attribute msg) -> Array ( Tuple String ( Html msg  )) -> Html msg
+node =
+  VirtualDom.keyedNode
+
+
+{-|-}
+ol :: forall msg. Array (Attribute msg) -> Array ( Tuple String ( Html msg  )) -> Html msg
+ol =
+  node "ol"
+
+
+{-|-}
+ul :: forall msg. Array (Attribute msg) -> Array (Tuple String ( Html msg  )) -> Html msg
+ul =
+  node "ul"
---
+++ src/Elm/Html/Lazy.purs
@@ -0,0 +1,48 @@
+module Elm.Html.Lazy ( lazy , lazy2 , lazy3 ) where
+
+{-| Since all Elm functions are pure we have a guarantee that the same input
+will always result in the same output. This module gives us tools to be lazy
+about building `Html` that utilize this fact.
+
+Rather than immediately applying functions to their arguments, the `lazy`
+functions just bundle the function and arguments up for later. When diffing
+the old and new virtual DOM, it checks to see if all the arguments are equal.
+If so, it skips calling the function!
+
+This is a really cheap test and often makes things a lot faster, but definitely
+benchmark to be sure!
+
+@docs lazy, lazy2, lazy3
+
+-}
+
+import Elm.Html (Html)
+import Elm.Native.VirtualDom as VirtualDom
+
+
+{-| A performance optimization that delays the building of virtual DOM nodes.
+
+Calling `(view model)` will definitely build some virtual DOM, perhaps a lot of
+it. Calling `(lazy view model)` delays the call until later. During diffing, we
+can check to see if `model` is referentially equal to the previous value used,
+and if so, we just stop. No need to build up the tree structure and diff it,
+we know if the input to `view` is the same, the output must be the same!
+
+-}
+lazy :: forall a msg. (a -> Html msg) -> a -> Html msg
+lazy =
+    VirtualDom.lazy
+
+
+{-| Same as `lazy` but checks on two arguments.
+-}
+lazy2 :: forall a b msg. (a -> b -> Html msg) -> a -> b -> Html msg
+lazy2 =
+    VirtualDom.lazy2
+
+
+{-| Same as `lazy` but checks on three arguments.
+-}
+lazy3 :: forall a b c msg. (a -> b -> c -> Html msg) -> a -> b -> c -> Html msg
+lazy3 =
+    VirtualDom.lazy3
