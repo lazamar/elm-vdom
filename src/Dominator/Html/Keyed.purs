a4f662579eba3b5ef6f513d3d377eff132e280d5
Marcelo Lazaroni <lazaronijunior@gmail.com>
2018-05-09 02:22:37 +0200

    Tidy up more documentation comments

--- src/Dominator/Html/Keyed.purs
+++ src/Dominator/Html/Keyed.purs
@@ -1,46 +1,35 @@
-module Dominator.Html.Keyed ( node , ol , ul ) where
-
-{-| A keyed node helps optimize cases where children are getting added, moved,
-removed, etc. Common examples include:
-
-  - The user can delete items from a list.
-  - The user can create new items in a list.
-  - You can sort a list based on name or date or whatever.
-
-When you use a keyed node, every child is paired with a string identifier. This
-makes it possible for the underlying diffing algorithm to reuse nodes more
-efficiently.
-
-# Keyed Nodes
-@docs node
-
-# Commonly Keyed Nodes
-@docs ol, ul
--}
-
+--| A keyed node helps optimize cases where children are getting added, moved,
+--| removed, etc. Common examples include:
+--| 
+--|   - The user can delete items from a list.
+--|   - The user can create new items in a list.
+--|   - You can sort a list based on name or date or whatever.
+--| 
+--| When you use a keyed node, every child is paired with a string identifier. This
+--| makes it possible for the underlying diffing algorithm to reuse nodes more
+--| efficiently.
+--| module Dominator.Html.Keyed ( node , ol , ul ) where
+module Dominator.Html.Keyed where
 
 import Dominator.Html (Attribute, Html)
 import Dominator.Core.VirtualDom as VirtualDom
 import Dominator.Operators (type (!))
 
 
-{-| Works just like `Html.node`, but you add a unique identifier to each child
-node. You want this when you have a list of nodes that is changing: adding
-nodes, removing nodes, etc. In these cases, the unique identifiers help make
-the DOM modifications more efficient.
--}
+--| Works just like `Html.node`, but you add a unique identifier to each child
+--| node. You want this when you have a list of nodes that is changing: adding
+--| nodes, removing nodes, etc. In these cases, the unique identifiers help make
+--| the DOM modifications more efficient.
 node :: ∀ msg. String -> Array (Attribute msg) -> Array (String ! Html msg) -> Html msg
 node =
   VirtualDom.keyedNode
 
 
-{-|-}
 ol :: ∀ msg. Array (Attribute msg) -> Array (String ! Html msg) -> Html msg
 ol =
   node "ol"
 
 
-{-|-}
 ul :: ∀ msg. Array (Attribute msg) -> Array (String ! Html msg) -> Html msg
 ul =
   node "ul"
--- src/Dominator/Operators.purs
+++ src/Dominator/Operators.purs
@@ -1,7 +1,28 @@
+--| Convenience functions for Tuple creation and function application
 module Dominator.Operators (module Pipe, (!), type (!)) where
 
 import Data.Function.Pipe as Pipe 
 import Data.Tuple (Tuple(Tuple))
 
-infixr 6 type Tuple as !
+
+--| PureScript doesn't have literal Tuple syntax. This infix Operator
+--| makes Tuple creation and descructuring look more similar to Haskell 
+--| and Elm tuple syntax.
+--|
+--| Here is an example
+--|```
+--| coordinates :: Tuple Int Int 
+--| coordinates = (5 ! 6)
+--|```
 infixr 0 Tuple as !
+
+
+--| Makes Tuple types look more similar to Haskell and Elm tuples.
+--|
+--| Example
+--|```
+--| coordinates :: Int ! Int 
+--| coordinates = Tuple 5 6
+--|```
+infixr 6 type Tuple as !
+
